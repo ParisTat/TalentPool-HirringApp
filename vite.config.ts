908a13bfa12d63440dd61525a0b85605c9bfe510
ParisTat <paris.tataridis@gmail.com>
2025-09-17 15:41:44 +0300

    Favicon and app icons working!

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild'
+  },
+  server: {
+    host: true,
+    port: 5173
+  }
+})
